cad0f1cb02f5379cfc764fea820e384f99a43169
Maithy Le <maithyle265@yahoo.com>
2022-09-15 22:15:23 -0700

    Added encoding url function

--- cc_main.py
+++ cc_main.py
@@ -1,4 +1,5 @@
 from cc_ui import welcome_message
+import cc_request
 
 
 import cc_ui as UI
@@ -6,3 +7,4 @@ import cc_ui as UI
 if __name__ == "__main__":
     UI.welcome_message()
     class_list = UI.prompt_classes()
+    #print(cc_request.encode_url(class_list))
